aa37abf7b399d1707038d45beaa14c173bd8cdfb
avishekflash <avisheksharmacoder@gmail.com>
2025-08-16 14:15:48 +0530

    Remove other files from the repo

--- src/src/settings_view.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-settings_controls = [
-    ft.Text("App Settings", size=30),
-    ft.Switch(label="Notifications"),
-    ft.Switch(label="Flash mode"),
-    ft.Switch(label="Save chats"),
-    ft.Switch(label="Use Local Liquid Models")
-]
